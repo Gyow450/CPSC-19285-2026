96c0155c551b1808b571cb83f805b95d2a0915ce
Gyow450 <1752708214@qq.com>
2026-08-03 20:00:05 +0800

    数字输入完成，缺文本处理

--- test_docx.py
+++ test_docx.py
@@ -6,14 +6,33 @@ from lxml import etree # type: ignore
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 TEMPLATE_PATH = os.path.join(BASE_DIR, r"docx_templates\模板.docx")
+M_NS = "http://schemas.openxmlformats.org/officeDocument/2006/math"
+
+def docx_edit(doc: DocumentObject, score:float, matrix: list[list] ,ver: list)-> DocumentObject:
+    matrix_values_list = [ x for row in matrix for x in row]
+    # v_values_list = [ x  for x in ver]
+    values_list = matrix_values_list + ver + [score]
+    idx =0 
+
+    for mt in doc.element.findall(f'.//{{{M_NS}}}t'): # type: ignore
+        if mt.text == 'PMR':
+            if idx < len(values_list):
+                mt.text = str(values_list[idx])
+                idx += 1
+            else:
+                break
+    return doc
 
 if __name__ == "__main__":
+    matirx = [
+        [1, 2, 3, 4],
+        [5, 6, 7, 8],
+        [9, 10, 11, 12],
+        [13, 14, 15, 16],
+        [17, 18, 19, 20],
+    ]
+    ver = [2, 4, 6, 8]
     doc = Document(TEMPLATE_PATH)
-    print(f"段落数量: {len(doc.paragraphs)}")
-    print(f"表格数量: {len(doc.tables)}")
-    for i, para in enumerate(doc.paragraphs[:10]):
-        text_preview = para.text[:60] if para.text else "(空或含公式)"
-        print(f"  段落{i}: {text_preview}")
-    M_NS = "http://schemas.openxmlformats.org/officeDocument/2006/math"
-    omath_count = len(doc.element.findall(f'.//{{{M_NS}}}oMath'))
-    print(f"\nOMML 公式 (<m:oMath>) 数量: {omath_count}")
+    docs = docx_edit(doc, 88.0,  matirx ,ver)
+    docs.save(os.path.join(BASE_DIR, "output.docx"))
+    
